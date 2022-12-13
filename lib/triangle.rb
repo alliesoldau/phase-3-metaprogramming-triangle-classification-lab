b1bbecf51a6758442c8e807babdb7756ac71881c
Allie Soldau <alliesoldau@gmail.com>
2022-12-13 12:32:48 -0500

    complete

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,43 @@
 class Triangle
-  # write code here
+  
+  attr_reader :arg1, :arg2, :arg3
+
+  def initialize(arg1, arg2, arg3)
+    @arg1 = arg1
+    @arg2 = arg2
+    @arg3 = arg3
+  end
+
+  def kind
+    validate_triangle
+    if arg1 == arg2 && arg2 == arg3
+      :equilateral
+    elsif arg1 == arg2 || arg1 == arg3 || arg2 == arg3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def validate_triangle
+    raise TriangleError unless all_args_positive? && side_sums_check_out?
+  end
+
+  def all_args_positive?
+    arg1 > 0 && arg2 > 0 && arg3 > 0
+    # alt solution
+    # [arg1, arg2, arg3].all?(&:positive?)
+  end
+
+  def side_sums_check_out?
+    arg1 + arg2 > arg3 && arg1 + arg3 > arg2 && arg2 + arg3 > arg1
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
+
+triangle = Triangle.new(1, 1, 1)
+triangle.kind
+
